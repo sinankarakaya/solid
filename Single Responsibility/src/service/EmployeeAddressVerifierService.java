b05b8b49da4f8a1b2ef9b389bdab981a96db5583
Sinan Karakaya <sinankarakaya1992@gmail.com>
2020-12-02 10:23:42 +0300

    Single Responsibility Add New Service

---
+++ Single Responsibility/src/service/EmployeeAddressVerifierService.java
@@ -0,0 +1,10 @@
+package service;
+
+import entity.EmployeeUser;
+
+public class EmployeeAddressVerifierService {
+    public boolean IsAddressValid(EmployeeUser user){
+        
+        return true;
+    }
+}
